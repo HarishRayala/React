5feb58c05aedc60fb5e206b15dd882bff4b565e5
HARISH RAYALA <100848582+HarishRayala@users.noreply.github.com>
2022-05-17 21:08:46 +0530

    react-navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar = () =>{
+    return (
+        <div className="bigcontainer">
+            <div className="smallcontainer">
+                <div className="tinycontainer">
+                    <h2>LOGOBAKERY</h2>
+                <div className="tinycontainer">
+                    <h3>Services</h3>
+                    <h3>Projects</h3>
+                    <h3>About</h3>
+                </div>
+                <div className="tinycontainer">
+                    <button>Contact</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+}
+export default Navbar;
