527bd16dfb1f0d45f736eedd3be750f5033eac79
HARISH RAYALA <100848582+HarishRayala@users.noreply.github.com>
2022-05-17 12:42:11 +0530

    React Mobile OS

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import Mobile  from './components/mobiles';
+function App() {
+  return (
+    <Mobile></Mobile>
+  );
+}
+export default App;
---
+++ src/components/mobiles.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Mobile = () =>{
+    return <div>
+        <h1>Mobile Operating System</h1>
+        <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>iphone</li>
+            <li>Windows Phone</li>
+        </ul>
+        <h1>Mobile Manufactures</h1>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </ul>
+    </div>
+}
+export default Mobile;
